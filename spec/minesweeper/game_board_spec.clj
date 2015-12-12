572e243e4b6ef0927e37f5f4f669ad98e74c2b4f
chopraapooja <all.is.well.chopra@gmail.com>
2015-12-12 19:39:03 +0530

    Gives mine-signal(-1) on openning mine cell.

--- spec/minesweeper/game_board_spec.clj
+++ spec/minesweeper/game_board_spec.clj
@@ -2,13 +2,11 @@
 	(:require [speclj.core :refer :all])
 	(:require [minesweeper.game-board :refer :all]))
 
-
-(def ^{:private true} board [[1 1][1 -1]])
+(def ^{:private true} board [[0 0]
+							[0 -1]])
 
 (describe "open-cell"
-
-	(it "opens cell of the given id & gives its content"
-    	(should= 1 (open-cell board {:row 0 :col 0}))
-    	(should= -1 (open-cell board {:row 1 :col 1}))))
+	(it "should give mine-signal(-1) on openning mine cell"
+    	(should= -1 (open-cell board :row 1 :col 1))))
 
 (run-specs)
