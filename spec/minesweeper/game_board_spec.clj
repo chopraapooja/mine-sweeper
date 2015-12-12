39428c4b2d5815f0933d7695841d5a46eaa14ac3
chopraapooja <all.is.well.chopra@gmail.com>
2015-12-12 17:17:22 +0530

    Added Todo

--- spec/minesweeper/game_board_spec.clj
+++ spec/minesweeper/game_board_spec.clj
@@ -3,17 +3,12 @@
 	(:require [minesweeper.game-board :refer :all]))
 
 
-(describe "get-board"
-
-	(it "get-board gives board"
-    	(should= [[1 1][1 -1]] (get-board))))
-
+(def ^{:private true} board [[1 1][1 -1]])
 
 (describe "open-cell"
 
-	(it "opens cell of given id & gives its content"
-    	(should= 1 (open-cell {:row 0 :col 0}))
-    	(should= -1 (open-cell {:row 1 :col 1}))))
-
+	(it "opens cell of the given id & gives its content"
+    	(should= 1 (open-cell board {:row 0 :col 0}))
+    	(should= -1 (open-cell board {:row 1 :col 1}))))
 
 (run-specs)
--- src/minesweeper/game_board.clj
+++ src/minesweeper/game_board.clj
@@ -1,9 +1,3 @@
 (ns minesweeper.game-board)
  
-(defn get-board []
-	[[1 1] [1 -1]])
-
-(defn open-cell [row-col] 
-	(nth 
-		(nth (get-board) (:row row-col)) 
-	(:col row-col)))
+(defn open-cell [board row-col] 0)
