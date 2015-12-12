1c7b80fbf3df0d92298fef17d8b0f856641c72cb
chopraapooja <all.is.well.chopra@gmail.com>
2015-12-12 19:46:31 +0530

    open-cell should not gives mine-signal(-1) on opening not mine cell

--- spec/minesweeper/game_board_spec.clj
+++ spec/minesweeper/game_board_spec.clj
@@ -6,7 +6,12 @@
 							[0 -1]])
 
 (describe "open-cell"
-	(it "should give mine-signal(-1) on openning mine cell"
-    	(should= -1 (open-cell board :row 1 :col 1))))
+	(it "should give mine-signal(-1) on opening mine cell"
+    	(should= -1 (open-cell board :row 1 :col 1)))
+	
+	(it "should not give mine-signal(-1) on opening not mine cell"
+    	(should-not= -1 (open-cell board :row 0 :col 0))
+    	(should-not= -1 (open-cell board :row 0 :col 1))
+    	(should-not= -1 (open-cell board :row 1 :col 0))))
 
 (run-specs)
