33efc7cc023ebab55025a5f8057ff88c8cda557c
chopraapooja <all.is.well.chopra@gmail.com>
2015-12-13 12:42:18 +0530

    Can find surrounding-cells for corner cell

--- src/minesweeper/game_board.clj
+++ src/minesweeper/game_board.clj
@@ -1,10 +1,19 @@
 (ns minesweeper.game-board)
 
-
 (defn open-cell [board & {:keys [row col] :as cell}]
 	(let [ cell-value (nth (nth board row) col)]
 		(if (neg? cell-value)
 			cell-value)))
 
 
-(defn surrounding-cells [cell])
+(defn surrounding-cells [ {:keys [row col]} ] 
+	[ 		
+			{:row (dec row) 	:col (dec col)}
+			{:row (dec row) 	:col col}
+			{:row (dec row) 	:col (inc col)}
+			{:row row 			:col (dec col)}
+			{:row row 			:col (inc col)}
+			{:row (inc row)		:col (dec col)}
+			{:row (inc row)		:col col}
+			{:row (inc row)		:col (inc col)} 
+	])
