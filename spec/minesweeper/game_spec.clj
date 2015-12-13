7690efc0edc6afc47283db019a258791f7219af2
chopraapooja <all.is.well.chopra@gmail.com>
2015-12-13 22:09:00 +0530

    1. Renamed board to board-view in Game 2. Can update-cell in game board view

--- spec/minesweeper/game_spec.clj
+++ spec/minesweeper/game_spec.clj
@@ -2,19 +2,19 @@
 	(:require [speclj.core :refer :all])
 	(:require [minesweeper.game :refer :all]))
 
-(def board [ [nil nil nil] [nil nil nil] [nil nil nil] ])
+(def board-view [ [nil nil nil] [nil nil nil] [nil nil nil] ])
 
 (describe "create"
 	(it "should give map"
-		(should= true (map? (create board)))
+		(should= true (map? (create board-view)))
 	)
 
 	(it "should map with get-status fn"
-		(should= true (fn? (:get-status (create board))))
+		(should= true (fn? (:get-status (create board-view))))
 	)
 
 	(it "should map with get-status fn"
-		(should= true (fn? (:get-status (create board))))
+		(should= true (fn? (:get-status (create board-view))))
 	)
 )
 
@@ -22,13 +22,30 @@
 	(it "should give initial status of game as all cells nil"
 		(should= 
 			[[nil nil nil] [nil nil nil] [nil nil nil]] 
-			((:get-status (create board))))
+			((:get-status (create board-view))))
 	)
 	(it "should give initial status of game as all cells nil"
 		(should= 
 			[[nil nil nil] [nil nil nil] [nil nil nil]] 
-			((:get-status (create board))))
+			((:get-status (create board-view))))
 	)
+)
+
+(describe "open-cell"
+	(it "opens cell at given location & returns new board-view"
+		(should= [[nil 1 nil] [nil nil nil] [nil nil nil]] (:get-status (:open-cell (create board-view))))
+	)
+)
+
+(describe "update-cell"
+	(it "updates cell at given location & returns new board-view"
+		(should= [[-1 nil nil] [nil nil nil] [nil nil nil]] (update-cell board-view {:row 0 :col 0} -1))
+	)
+
+	(it "should give back same board-view when cell location is not valid"
+		(should= [[nil nil nil] [nil nil nil] [nil nil nil]] (update-cell board-view {:row 3 :col 3} -1))
+	)
+
 )
 
 (run-specs)
--- src/minesweeper/game.clj
+++ src/minesweeper/game.clj
@@ -1,7 +1,26 @@
-(ns minesweeper.game)
+(ns minesweeper.game
+	(:require [minesweeper.game-board :as game-board]))
 
-(defn get-status [board]
-	board)
+(defn get-status [board] board)
 
-(defn create [board]
-	{ :get-status  (partial get-status board)} )
+(def ^{:private true} board [[-1 0 0]		; [-1 1 0]
+							 [0  0 0]		; [2  2 1]
+							 [0 -1 0]])		; [1 -1 1]
+
+(defn update-row [row in-dex new-value]
+	(map-indexed (fn [i x] (if (= i in-dex) new-value x)) row)
+)
+
+(defn update-cell [board-view {:keys [row col] :as cell} new-value]
+	(if (not (game-board/is-valid? board-view cell))
+		board-view
+		(map-indexed (fn [i r] (if (= i row) (update-row r col new-value) r)) board-view))
+)
+
+(defn open-cell [board-view & {:keys [row col] :as cell}]
+	; ( update-cell board-view cell (game-board/open-cell board cell) )
+)
+
+(defn create [board-view]
+	{ :get-status  (partial get-status board-view)
+	:open-cell (partial open-cell board-view)} )
