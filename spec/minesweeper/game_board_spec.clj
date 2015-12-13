c43fa2a231dff716016a1b4a33b7ffaa98a349a0
chopraapooja <all.is.well.chopra@gmail.com>
2015-12-13 12:12:20 +0530

    Changed test-board to size 3*3 WIP

--- spec/minesweeper/game_board_spec.clj
+++ spec/minesweeper/game_board_spec.clj
@@ -2,16 +2,30 @@
 	(:require [speclj.core :refer :all])
 	(:require [minesweeper.game-board :refer :all]))
 
-(def ^{:private true} board [[0 0]
-							[0 -1]])
+(def ^{:private true} board [[-1 0 0]		; [-1 1 0]
+							 [0  0 0]		; [2  2 1]
+							 [0 -1 0]])		; [1 -1 1]
 
 (describe "open-cell"
-	(it "should give mine-signal(-1) on opening mine cell"
-    	(should= -1 (open-cell board :row 1 :col 1)))
+	(it "should give bomb-signal(-1) on opening bomb cell"
+    	(should= -1 (open-cell board :row 0 :col 0)))
 	
-	(it "should not give mine-signal(-1) on opening not mine cell"
-    	(should-not= -1 (open-cell board :row 0 :col 0))
+	(it "should not give bomb-signal(-1) on opening not bomb cell"
     	(should-not= -1 (open-cell board :row 0 :col 1))
-    	(should-not= -1 (open-cell board :row 1 :col 0))))
+    	(should-not= -1 (open-cell board :row 0 :col 2))
+    	(should-not= -1 (open-cell board :row 1 :col 0))
+    	(should-not= -1 (open-cell board :row 1 :col 1))
+    	(should-not= -1 (open-cell board :row 1 :col 2))
+    	(should-not= -1 (open-cell board :row 2 :col 0))
+    	(should-not= -1 (open-cell board :row 2 :col 2)))
+
+	(it "should give number of bombs surround cell"
+    	(should= 1 (open-cell board :row 1 :col 0))))
+
+(describe "surrounding-cells"
+
+	(it "surrounding-cells of 8 cells for centered cell"
+		((should= 8 (surrounding-cells {:row 1 :col 1})))))
+
 
 (run-specs)
--- src/minesweeper/game_board.clj
+++ src/minesweeper/game_board.clj
@@ -1,4 +1,10 @@
 (ns minesweeper.game-board)
 
-(defn open-cell [board & {:keys [row col]}]
-	(nth (nth board row) col))
+
+(defn open-cell [board & {:keys [row col] :as cell}]
+	(let [ cell-value (nth (nth board row) col)]
+		(if (neg? cell-value)
+			cell-value)))
+
+
+(defn surrounding-cells [cell])
